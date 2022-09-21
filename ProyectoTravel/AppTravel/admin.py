5d8d11d4dde654d2f4812a04c0728b4ea505dc7e
Falton09 <francopelayo09@gmail.com>
2022-09-21 11:39:11 -0300

    dejando en funcionamiento el admin

--- ProyectoTravel/AppTravel/admin.py
+++ ProyectoTravel/AppTravel/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from AppTravel.models import NombreDestino, NombreHotel, Vuelo, Hotel
+from UserTravel.models import Avatar, Testimonio
 
-# Register your models here.
+#Admin de AppTravel
+admin.site.register(Vuelo)
+admin.site.register(Hotel)
+admin.site.register(NombreDestino)
+admin.site.register(NombreHotel)
+
+
+#Admin de UserTravel
+admin.site.register(Avatar)
+admin.site.register(Testimonio)
