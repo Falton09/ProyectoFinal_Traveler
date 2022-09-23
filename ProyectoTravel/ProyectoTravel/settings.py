a7aa3c1aa2ed835cdd026b64094f438d585664c9
Falton09 <francopelayo09@gmail.com>
2022-09-23 08:43:24 -0300

    instalando ckeditor

--- ProyectoTravel/ProyectoTravel/settings.py
+++ ProyectoTravel/ProyectoTravel/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AppTravel',
-    'UserTravel'
+    'UserTravel',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ProyectoTravel/UserTravel/models.py
+++ ProyectoTravel/UserTravel/models.py
@@ -1,14 +1,20 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Avatar(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to='avatares', null=True, blank=True)
+    imagen = models.ImageField(default="default.png",upload_to='avatares', null=False)
 
     def __str__(self):
        return f"{self.user}"
 
 class Testimonio(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    texto = models.TextField(max_length=50)
+    texto = RichTextField()
+    fecha_publicacion = models.DateTimeField(default=datetime.now())
 
+# class ComentarioTestimonio(models.Model):
+#     id_testimonio = models.ForeignKey(Testimonio, on_delete=models.CASCADE, null=True)
+#     user_comentario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
